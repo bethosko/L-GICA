48bf659efbcee1de62d11d7fe145d30d87fb9a1a
bethosko <bethosko@gmail.com>
2025-04-23 16:51:38 -0300

    imc

---
+++ imc.js
@@ -0,0 +1,8 @@
+let peso = parseInt(readline.question("qual seu peso? "));
+let altura = readline.question("qual sua altura? ");
+let
+if(indice<18,5){
+    console.log("abaixo do peso")
+}else if(indice<=18 || idade >=65 || carteira=="s"){
+    console.log("meia")
+    
