ad87e29e7dffa65ccbc485ffa1f50905479ba678
bethosko <bethosko@gmail.com>
2025-04-24 15:51:07 -0300

    imc_pronto

--- imc.js
+++ imc.js
@@ -1,8 +1,16 @@
-let peso = parseInt(readline.question("qual seu peso? "));
-let altura = readline.question("qual sua altura? ");
-let
-if(indice<18,5){
-    console.log("abaixo do peso")
-}else if(indice<=18 || idade >=65 || carteira=="s"){
-    console.log("meia")
+const readline = require("readline-sync");
+let peso = parseInt (readline.question("qual seu peso? "));
+let altura = parseFloat(readline.question("Qual sua altura? "));
+let IMC= peso/(altura * altura);
+console.log("Seu IMC é:"+IMC);
+if(IMC <18.5){
+    console.log("só a grade")
+}else if(IMC >=18.5 && IMC <=24.9){
+    console.log("Normal")
+}else if(IMC >=25.0 && IMC <=30){
+    console.log("Sobrepeso")
+}else if(IMC >=30.1 && IMC <=39.9){
+    console.log("Obeso")
+}else{
+    console.log("Obeso-Morbido")}
     
